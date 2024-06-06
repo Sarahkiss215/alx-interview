4f06a7530cc0fb184d117588a4a709f8186fd16e
Sarahkiss215 <skisiangani5@gmail.com>
2024-06-06 07:43:49 +0000

    Correct pycodestyle errors

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-"""Determines the fewest number of coins needed to meet a given amount total."""
+"""
+Determines the fewest number of coins
+needed to meet a given amount total
+"""
 
 
 def makeChange(coins, total):
